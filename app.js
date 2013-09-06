aac5bf1c991567bb940079e85c2c715e319aea6c
iamblue <tonyone0902@gmail.com>
2013-09-06 12:15:05 +0800

    iiii

--- app.js
+++ app.js
@@ -36,4 +36,5 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 console.log('kerker');
+console.log('kerker');
 
